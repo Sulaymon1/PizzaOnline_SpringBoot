027724e65801232312be07c676d45450e6359300
Roman <klimov_ru@rambler.ru>
2018-10-01 21:38:24 +0300

    +cart controller

---
+++ src/main/java/ru/pizza/controllers/CartController.java
@@ -0,0 +1,46 @@
+package ru.itis.pizzaonline.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import ru.itis.pizzaonline.models.Cart;
+import ru.itis.pizzaonline.models.User;
+import ru.itis.pizzaonline.repositories.CartRepository;
+import ru.itis.pizzaonline.services.implementations.AuthenticationServiceImpl;
+import ru.itis.pizzaonline.services.implementations.CartServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CartController {
+
+    @Autowired
+    private AuthenticationServiceImpl service;
+
+    @Autowired
+    private CartServiceImpl cartService;
+
+
+    @GetMapping("/cart")
+    public String getAllClientPizza( ModelMap modelMap, Authentication authentication){
+        User user = service.getUserByAuthentication(authentication);
+        List<Cart> carts = cartService.getAllPizzas(user);
+        System.out.println(Arrays.asList(carts));
+        modelMap.addAttribute("carts", carts);
+        return "client/cart";
+    }
+
+    @GetMapping("/deleteFromCart")
+    public String fromCart(@PathVariable(value = "cartId", required = true) Long id,Authentication authentication) {
+
+            if (authentication != null) {
+                User user = service.getUserByAuthentication(authentication);
+                cartService.deleteCartByIDandUser(id, user);
+            }
+
+        return "redirect:/cart";
+    }
+}
